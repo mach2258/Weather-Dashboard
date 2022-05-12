f6a9473534763ea3263324a1f9db11e6fb47866d
Erich Elste <erichelste@yahoo.com>
2022-05-12 02:06:01 -0400

    added html, css and started css

---
+++ script.js
@@ -0,0 +1,10 @@
+var searchHistory = []
+var apiKey = '4f3fcca94c389f03e28b2ab94f5cbc9a'
+var rootUrl = 'https://api.openweathermap.org';
+
+var searchForm = document.querySelector('#searchform')
+var searchBar = document.querySelector('#searchBar')
+var historyList = document.querySelector('#historyList')
+var today = document.querySelector('#city')
+var forcast5day = document.querySelector('#forcast')
+
